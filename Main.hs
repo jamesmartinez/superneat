691b57062d55e5f1c72502932334927ec6b34f84
Sarah Brofeldt <sarah@thinkmonster.(none)>
2013-03-03 22:39:46 +0100

    use copyFile so that it works across different devices

--- Main.hs
+++ Main.hs
@@ -22,7 +22,7 @@ import Happstack.Server ( nullConf,
 import Text.Blaze ((!))
 import Text.Blaze.Html5 as H
 import qualified Text.Blaze.Html5.Attributes as A
-import System.Directory (renameFile)
+import System.Directory (copyFile)
 
 main = simpleHTTP nullConf $ msum
          [ dir "static" $ serveDirectory EnableBrowsing [] "static"
@@ -58,7 +58,7 @@ uploadForm =
 uploadPost = do
     decodeBody (defaultBodyPolicy "/tmp/" (10*10^8) 1000 1000)
     (p,n,ct) <- lookFile "file_upload"
-    liftIO $ renameFile p ("static/" ++ n)
+    liftIO $ copyFile p ("static/" ++ n)
     ok $ toResponse $ template "ok" $ toHtml $ do
         H.a ! A.href "static/" $ "Gallery"
         
