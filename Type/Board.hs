d061a209dc63f806c58c7701fafc771936d707ab
Sarah Brofeldt <sbrofeldt@gmail.com>
2013-03-09 00:12:04 +0100

    Fix imports from Board

--- Type/Board.hs
+++ Type/Board.hs
@@ -1,11 +1,6 @@
 module Type.Board where
 
 import Type.Common
-
-newtype UserId = UserId {unUserId :: Integer }
-               deriving (Eq, Ord, Num, Typeable, SafeCopy)
-
-newtype PinId = PinId { unPinId :: Integer }
-              deriving (Eq, Ord, Num, Enum, Typeable, Safecopy)
+import Type.Pin (UserId, PinId)
 
 data Board = Board { boardPins :: [PinId], boardOwner :: UserId, followers :: [UserId] }
