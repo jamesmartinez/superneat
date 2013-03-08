120f94d7c08f1e98eebfe5b1ab5f68e2c7def4fc
jamesmartinez <elloromtz@gmail.com>
2013-03-08 12:11:20 -0600

    Added some boilerplate for Type/Board.hs that Type/Pin.hs has

---
+++ Type/Board.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE TemplateHaskell, DeriveDataTypeable, GeneralizedNewtypeDeriving, RecordWildCards, TypeFamilies, EmptyDataDecls #-}
+module Type.Board where
+
+import Data.Text as Text (Text, empty)
+import Data.Time.Clock (UTCTime)
+import Data.Data (Typeable)
+import Data.SafeCopy (SafeCopy, deriveSafeCopy, base)
+import Data.Acid (Update, Query, makeAcidic)
+import Control.Monad.State (get, put)
+import Control.Monad.Reader (ask)
+import Data.IxSet as IxSet (Indexable, IxSet, empty, ixSet, ixFun, insert, (@=), getOne, updateIx, toList)
+
+-- All the crunchy stuff
+
